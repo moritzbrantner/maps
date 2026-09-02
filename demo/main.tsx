10551868a5636e2ceda86798d167826145c9c82d
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-02 18:27:17 +0200

    fix: restore demo cascade order

--- demo/main.tsx
+++ demo/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import "../styles.css";
 import "./styles.css";
+import "../styles.css";
 
 import { App } from "./App";
 
