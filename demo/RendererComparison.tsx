799ac4c599bf015e095d24838283134f5f2b2ba5
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-19 04:36:52 +0200

    Format renderer comparison

--- demo/RendererComparison.tsx
+++ demo/RendererComparison.tsx
@@ -13,7 +13,10 @@ import {
 } from "@moritzbrantner/maps";
 import type { MapsCanvasFlatRuntimeController } from "../src/canvas-flat-runtime";
 import { demoMapStyle } from "./data/map-style";
-import { getShortbreadBasemapStyle, useShortbreadBasemap } from "./ShortbreadBasemapLayer";
+import {
+  getShortbreadBasemapStyle,
+  useShortbreadBasemap,
+} from "./ShortbreadBasemapLayer";
 
 type RendererBackend = "maps" | "maplibre";
 
